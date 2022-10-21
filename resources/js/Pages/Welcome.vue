e79c16df545d9aaf82dced670b24ac9c21463acc
tcampbPPU <tcamp022@gmail.com>
2022-10-20 21:56:22 -0400

    wip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,36 +1,62 @@
 <script lang="ts" setup>
-import {computed, PropType} from 'vue'
+import { computed, PropType } from 'vue'
 import route from 'ziggy-js'
-import { Head, Link, usePage } from '@inertiajs/inertia-vue3';
+import { Head, Link, usePage } from '@inertiajs/inertia-vue3'
 
 defineProps({
     canLogin: Boolean as PropType<boolean>,
     canRegister: Boolean as PropType<boolean>,
     laravelVersion: String as PropType<string>,
     phpVersion: String as PropType<string>,
-});
+})
 
 const user = computed(() => {
-    return (usePage().props.value as inertia.Props).user;
-});
+    return (usePage().props.value as inertia.Props).user
+})
 </script>
 
 <template>
     <Head title="Welcome" />
 
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
+    <div
+        class="items-top relative flex min-h-screen justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    >
+        <div
+            v-if="canLogin"
+            class="fixed top-0 right-0 hidden px-6 py-4 sm:block"
+        >
+            <Link
+                v-if="user"
+                :href="route('dashboard')"
+                class="text-sm text-gray-700 underline dark:text-gray-500"
+            >
+                Dashboard
+            </Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
+                <Link
+                    :href="route('login')"
+                    class="text-sm text-gray-700 underline dark:text-gray-500"
+                >
+                    Log in
+                </Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 text-sm text-gray-700 underline dark:text-gray-500"
+                >
+                    Register
+                </Link>
             </template>
         </div>
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+        <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
+            <div
+                class="mt-4 flex justify-center sm:items-center sm:justify-between"
+            >
+                <div
+                    class="ml-4 text-center text-sm text-gray-500 sm:ml-0 sm:text-right"
+                >
                     Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
                 </div>
             </div>
